a5f6139773a66ca896ac996be23a1c34369de913
Matrej <blaha.matej@gmail.com>
2023-02-08 22:46:30 +0100

    Update README file, make each "pixel" a square, style the background to black while the gridElements start as whiteo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etch-a-sketch
 another project from The Odin project
+
+The issue in this exercise was to create a grid of divs, which will react on mouseover movement. If you hover over the element, it will darken 10% black to make it completely black. You can reset grid from default 16x16 (WxH by screen size) to prompt another resolution of a new grid.
+
+You can type max resolution of 100 x 100.
--- script.js
+++ script.js
@@ -1,44 +1,51 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset');
 
-function makeGrid(columns, rows) {
-    for (let y = 0; y < rows; y++) {
+let light = [];
+makeGrid(16);
+
+resetButton.addEventListener('click', (e) => resetGrid(prompt('Choose the resolution for a new grid!')));
+
+function makeGrid(resolution) {
+    for (let y = 0; y < resolution; y++) {
         let row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        for (let x = 0; x < columns; x++) {
+        for (let x = 0; x < resolution; x++) {
             let column  = document.createElement('div');
             column.classList.add('gridItem', 'column');
             row.appendChild(column);
-            column.style.width = `${window.innerWidth / columns}px`;
-            column.style.height = `${(window.innerHeight - 150) / rows}px`;
+            let pixelSize = (window.innerHeight - 200) / resolution;
+            column.style.width = `${pixelSize}px`;
+            column.style.height = `${pixelSize}px`;
         }
     }
     recolorGridItems();
 }
 
-let light = [];
-makeGrid(16,16);
-
 function recolorItem (item, index) {
+    // RANDOM COLOR FOR BACKGROUND OF EVERY MOUSEOVER ELEMENTS
     //item.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    item.target.style.backgroundColor = `hsl(0, 0%, ${light[index]}%)`;
+
     light[index] -= 10;
+    item.target.style.backgroundColor = `hsl(0, 0%, ${light[index]}%)`;
     if (light[index] < 0) {
         light[index] = 0;
     }
 }
-resetButton.addEventListener('click', (e) => resetGrid(prompt('how many columns?'),prompt('how many rows?')));
 
-function resetGrid (numColumns, numRows) {
-    if (numColumns >= 100 || numRows >= 100) {
+function resetGrid (resolution) {
+    if (resolution > 100) {
         alert('maximum number is set to 100, try again!');
+        return;
+    } else if (resolution < 1) {
+        alert('minimum number is set to 1, try again!');
         return;
     }
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
     };
-    makeGrid(numColumns, numRows);
+    makeGrid(resolution);
 }
 function recolorGridItems() {
     const gridItems = document.querySelectorAll('.gridItem');
