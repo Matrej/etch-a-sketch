68b45c08f6d0ed371d24990c207be4f00f82c183
Matrej <blaha.matej@gmail.com>
2023-02-08 21:56:33 +0100

    Finish the script for recoloring each div and function behind reseting grid. B&W version

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('.reset');
 
 function makeGrid(columns, rows) {
-    console.log(columns + ', ' + rows);
-    const array = new Array;
     for (let y = 0; y < rows; y++) {
         let row = document.createElement('div');
         row.classList.add('row');
@@ -11,9 +10,40 @@ function makeGrid(columns, rows) {
             let column  = document.createElement('div');
             column.classList.add('gridItem', 'column');
             row.appendChild(column);
+            column.style.width = `${window.innerWidth / columns}px`;
+            column.style.height = `${(window.innerHeight - 150) / rows}px`;
         }
     }
-    return array;
+    recolorGridItems();
 }
 
+let light = [];
 makeGrid(16,16);
+
+function recolorItem (item, index) {
+    //item.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    item.target.style.backgroundColor = `hsl(0, 0%, ${light[index]}%)`;
+    light[index] -= 10;
+    if (light[index] < 0) {
+        light[index] = 0;
+    }
+}
+resetButton.addEventListener('click', (e) => resetGrid(prompt('how many columns?'),prompt('how many rows?')));
+
+function resetGrid (numColumns, numRows) {
+    if (numColumns >= 100 || numRows >= 100) {
+        alert('maximum number is set to 100, try again!');
+        return;
+    }
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    };
+    makeGrid(numColumns, numRows);
+}
+function recolorGridItems() {
+    const gridItems = document.querySelectorAll('.gridItem');
+    gridItems.forEach((item, index) => {
+        light[index] = 100;
+        item.addEventListener('mouseover', (e) => recolorItem(e, index))
+    });
+};
